ef02acb715783f89c2bf0514d9025a446895a37e
Guanlin Chen <lolitsundere@hotmail.com>
2018-06-11 15:11:27 +0800

    修改Relics类

--- Assets/Scripts/Relics.cs
+++ Assets/Scripts/Relics.cs
@@ -4,4 +4,18 @@ using System.Linq;
 
 public abstract class Relics
 {
+    /// <summary>
+    /// 圣物名称
+    /// </summary>
+    public String RelicsName { get; set; }
+
+    /// <summary>
+    /// 圣物图片文件名
+    /// </summary>
+    public String RelicsSpriteName { get; set; }
+
+    /// <summary>
+    /// 圣物效果
+    /// </summary>
+    public Effect RelicsEffect { get; set; }
 }
