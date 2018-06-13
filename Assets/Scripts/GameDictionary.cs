6586f7b624b971d80840d19c6e59975bf51af7ec
Guanlin Chen <lolitsundere@hotmail.com>
2018-06-13 18:07:39 +0800

    新增4个类方便Json读取

---
+++ Assets/Scripts/GameDictionary.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class GameDictionary
+{
+    /// <summary>
+    /// 游戏所有卡牌信息
+    /// </summary>
+    public Dictionary<string, CardTransferModel> CardDic { get; set; }
+
+    /// <summary>
+    /// 游戏所有英雄信息
+    /// </summary>
+    public Dictionary<string, Hero> HeroDic { get; set; }
+}
+
---
+++ Assets/Scripts/PlayerTransferModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class PlayerTransferModel
+{
+    /// <summary>
+    /// 玩家名称
+    /// </summary>
+    public string PlayerName { get; set; }
+
+    /// <summary>
+    /// 账号名称
+    /// </summary>
+    public string AccountName { get; set; }
+
+    /// <summary>
+    /// 密码
+    /// </summary>
+    public string Password { get; set; }
+
+    /// <summary>
+    /// 玩家拥有英雄
+    /// </summary>
+    public List<CardTransferModel> PlayerHeroList { get; set; }
+}
+
